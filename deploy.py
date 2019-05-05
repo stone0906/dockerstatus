7b725dc056802a16af721d56dde05cff3a39a607
stone0906 <43129239+stone0906@users.noreply.github.com>
2019-05-05 19:01:25 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,8 +2,9 @@
 import sys
 import os
 
-if sys.argv.__len__() == 1:
-    print("need one arg for name")
+if sys.argv.__len__() <= 2:
+    print("need two arg for name")
+    print("like python deployment.py 阿里云节点 58.23.42.123")
     exit(0)
 os.chdir("/root/")
 with open("docker_info.sh", 'w') as f:
@@ -25,10 +26,16 @@ neto=$(echo ${result} |awk '{print $10}')
 curl -G --data-urlencode "name=$MY_NAME" "$TMPSTR:1980/?KEY=hentaidoor&ttl=$ttl&cpu=${cpu:0:-1}&mem=${mem:0:-1}&inet=$neti&onet=$neto&id="$var"_"$name
 
 done''' % (sys.argv[1]))
-os.system("echo -e '\n58.33.69.246    T_docker' >> /etc/hosts")
+os.system("echo -e \n >> /etc/hosts")
+os.system("echo '%s    T_docker' >> /etc/hosts" % (sys.argv[2]))
 os.system('(echo "* * * * * sleep 50 && /bin/bash /root/docker_info.sh"; crontab -l )| crontab')
 os.system('(echo "* * * * * sleep 40 && /bin/bash /root/docker_info.sh"; crontab -l )| crontab')
 os.system('(echo "* * * * * sleep 30 && /bin/bash /root/docker_info.sh"; crontab -l )| crontab')
 os.system('(echo "* * * * * sleep 20 && /bin/bash /root/docker_info.sh"; crontab -l )| crontab')
 os.system('(echo "* * * * * sleep 10 && /bin/bash /root/docker_info.sh" ; crontab -l )| crontab')
 os.system('(echo "* * * * * /bin/bash /root/docker_info.sh"; crontab -l )| crontab')
+
+content = os.popen('cat /etc/hosts')
+print(content.read())
+content = os.popen('crontab -l')
+print(content.read())
